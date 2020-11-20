1b7364bf94c60a168799979a9255c4c6f48702c1
masayoshi64 <sho32001025@gmail.com>
2020-11-20 20:02:02 +0900

    verify lazysegtree

---
+++ verify/aoj.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM \
+    "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_G&lang=ja"
+
+#include "library/template/template.cpp"
+// library
+#include "library/structure/segtree/LazySegmentTree.cpp"
+int main() {
+    int n, q;
+    cin >> n >> q;
+    RSRAQ<ll> seg(n);
+    rep(i, q) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int s, t, x;
+            cin >> s >> t >> x;
+            s--;
+            seg.update(s, t, x);
+        } else {
+            int s, t;
+            cin >> s >> t;
+            s--;
+            print(seg.query(s, t));
+        }
+    }
+}
