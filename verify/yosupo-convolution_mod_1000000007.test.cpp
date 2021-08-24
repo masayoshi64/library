f3f9552b2c06b2081cc54d4786d14d4617783c81
masayoshi64 <sho32001025@gmail.com>
2021-08-24 21:16:16 +0900

    verify

--- verify/yosupo-convolution_mod_1000000007.test.cpp
+++ verify/yosupo-convolution_mod_1000000007.test.cpp
@@ -1,26 +1,20 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod_1000000007"
 #include "library/template/template.cpp"
 // library
-#include "library/convolution/NTT.cpp"
 #include "library/mod/modint.cpp"
 //
 #include "library/convolution/FFT.cpp"
 #include "library/math/FormalPowerSeries.cpp"
 using mint = modint<1000000007>;
-using FPS = FormalPowerSeries<mint>;
-FPS mult_fft(const FPS::P& a, const FPS::P& b) {
-    auto ret = FFT::multiply(a, b);
-    return FPS::P(ret.begin(), ret.end());
-}
-// FPS::set_fft(mult_ntt); in main
-int main() {
+FFT fft;
+int main()
+{
     int n, m;
     cin >> n >> m;
-    FPS a(n), b(m);
+    vector<mint> a(n), b(m);
     rep(i, n) cin >> a[i];
     rep(i, m) cin >> b[i];
-    FPS::set_fft(mult_fft);
-    auto c = a * b;
+    auto c = fft.multiply(a, b);
     rep(i, n + m - 1) { cout << c[i] << ' '; }
     cout << endl;
 }
