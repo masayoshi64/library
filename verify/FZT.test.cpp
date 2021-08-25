11d9c1ff6af33322ffef2e3632565375b6c94351
masayoshi64 <sho32001025@gmail.com>
2021-08-25 10:33:17 +0900

    rename

--- verify/FZT.test.cpp
+++ verify/FZT.test.cpp
@@ -3,7 +3,8 @@
 #include "library/template/template.cpp"
 // library
 #include "library/convolution/FZT.cpp"
-int main() {
+int main()
+{
     cin.tie(0);
     ios::sync_with_stdio(0);
     cout << setprecision(30) << fixed;
@@ -14,32 +15,35 @@ int main() {
     scan(a);
     rep(i, n) cin >> p[i], p[i] /= 100;
     vector<ll> f(1 << n);
-    rep(s, 1 << n) {
+    rep(s, 1, 1 << n)
+    {
         ll prod = 1;
-        rep(i, n) {
-            if ((s & (1 << i)) == 0) continue;
+        rep(i, n)
+        {
+            if ((s & (1 << i)) == 0)
+                continue;
             ll g = gcd(prod, a[i]);
-            if ((__int128_t)prod * a[i] / g <= m)
+            if ((__int128_t)prod * (a[i] / g) <= m)
                 prod = prod * (a[i] / g);
             else
                 prod = m + 1;
         }
-        if (s == 0) continue;
         f[s] = (popcnt(s) % 2 ? 1 : -1) * (m / prod);
     }
     // FZTで包除原理
     auto g = FZT(f);
     ld ans = 0;
     // 期待値計算
-    rep(s, 1 << n) {
+    rep(s, 1 << n)
+    {
         ld ps = 1;
-        rep(i, n) {
+        rep(i, n)
+        {
             if ((s & (1 << i)) == 0)
                 ps *= 1 - p[i];
             else
                 ps *= p[i];
         }
-        ll tmp = g[s];
         ans += ps * g[s];
     }
     print(ans);
