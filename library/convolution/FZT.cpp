4db8aa68e9a53868b0fabc86a68877772dcee3f8
masayoshi64 <sho32001025@gmail.com>
2021-08-25 10:21:14 +0900

    add doxygen

--- library/convolution/FZT.cpp
+++ library/convolution/FZT.cpp
@@ -1,13 +1,21 @@
-// 部分集合に関する和
-// 上位集合に関する和にもできる（s->(1<<n)-1-sにするだけ)
+/**
+ * @brief Fast Zeta Transform
+ * @docs docs/FZT.md
+ * @details 全ての部分集合$T_i$について$\sum_{S \subset T_i}f(S)$を計算
+ */
 template <typename T>
-vector<T> FZT(vector<T> f) {
+vector<T> FZT(vector<T> f)
+{
     int N = 0;
     int tmp = f.size();
-    while (tmp > 1) N++, tmp /= 2;
-    for (int j = 0; j < N; j++) {
-        for (int i = 0; i < (1 << N); i++) {
-            if (i & (1 << j)) {
+    while (tmp > 1)
+        N++, tmp /= 2;
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < (1 << N); i++)
+        {
+            if (i & (1 << j))
+            {
                 f[i] += f[i & ~(1 << j)];
             }
         }
