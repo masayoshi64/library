638a362d89e1df0f053b0819d84131b695e7b111
masayoshi64 <sho32001025@gmail.com>
2021-08-26 10:32:17 +0900

    add pragma once

--- library/mod/modint.cpp
+++ library/mod/modint.cpp
@@ -1,48 +1,58 @@
+#pragma once
 template <int Mod>
-struct modint {
+struct modint
+{
     int x;
 
     modint() : x(0) {}
 
     modint(long long y) : x(y >= 0 ? y % Mod : (Mod - (-y) % Mod) % Mod) {}
 
-    modint& operator+=(const modint& p) {
-        if ((x += p.x) >= Mod) x -= Mod;
+    modint &operator+=(const modint &p)
+    {
+        if ((x += p.x) >= Mod)
+            x -= Mod;
         return *this;
     }
 
-    modint& operator-=(const modint& p) {
-        if ((x += Mod - p.x) >= Mod) x -= Mod;
+    modint &operator-=(const modint &p)
+    {
+        if ((x += Mod - p.x) >= Mod)
+            x -= Mod;
         return *this;
     }
 
-    modint& operator*=(const modint& p) {
+    modint &operator*=(const modint &p)
+    {
         x = (int)(1LL * x * p.x % Mod);
         return *this;
     }
 
-    modint& operator/=(const modint& p) {
+    modint &operator/=(const modint &p)
+    {
         *this *= p.inverse();
         return *this;
     }
 
     modint operator-() const { return modint(-x); }
 
-    modint operator+(const modint& p) const { return modint(*this) += p; }
+    modint operator+(const modint &p) const { return modint(*this) += p; }
 
-    modint operator-(const modint& p) const { return modint(*this) -= p; }
+    modint operator-(const modint &p) const { return modint(*this) -= p; }
 
-    modint operator*(const modint& p) const { return modint(*this) *= p; }
+    modint operator*(const modint &p) const { return modint(*this) *= p; }
 
-    modint operator/(const modint& p) const { return modint(*this) /= p; }
+    modint operator/(const modint &p) const { return modint(*this) /= p; }
 
-    bool operator==(const modint& p) const { return x == p.x; }
+    bool operator==(const modint &p) const { return x == p.x; }
 
-    bool operator!=(const modint& p) const { return x != p.x; }
+    bool operator!=(const modint &p) const { return x != p.x; }
 
-    modint inverse() const {
+    modint inverse() const
+    {
         int a = x, b = Mod, u = 1, v = 0, t;
-        while (b > 0) {
+        while (b > 0)
+        {
             t = a / b;
             swap(a -= t * b, b);
             swap(u -= t * v, v);
@@ -50,21 +60,26 @@ struct modint {
         return modint(u);
     }
 
-    modint pow(int64_t n) const {
+    modint pow(int64_t n) const
+    {
         modint ret(1), mul(x);
-        while (n > 0) {
-            if (n & 1) ret *= mul;
+        while (n > 0)
+        {
+            if (n & 1)
+                ret *= mul;
             mul *= mul;
             n >>= 1;
         }
         return ret;
     }
 
-    friend ostream& operator<<(ostream& os, const modint& p) {
+    friend ostream &operator<<(ostream &os, const modint &p)
+    {
         return os << p.x;
     }
 
-    friend istream& operator>>(istream& is, modint& a) {
+    friend istream &operator>>(istream &is, modint &a)
+    {
         long long t;
         is >> t;
         a = modint<Mod>(t);
