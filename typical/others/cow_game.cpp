f008fe08f54ad291e6932b94ee3e11a88ba4106e
masayoshi64 <sho32001025@gmail.com>
2021-08-30 21:12:55 +0900

    add 牛ゲー

---
+++ typical/others/cow_game.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://atcoder.jp/contests/abc216/tasks/abc216_g"
+#include "library/graph/distance/dijkstra.cpp"
+#include "library/template/template.cpp"
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    Graph<ll> g(n + 1);
+    rep(i, n) {
+        g.add_directed_edge(i, i + 1, 1);
+        g.add_directed_edge(i + 1, i, 0);
+    }
+    rep(i, m) {
+        ll l, r, x;
+        cin >> l >> r >> x;
+        l--;
+        g.add_directed_edge(l, r, r - l - x);
+    }
+    auto dist = dijkstra(g, 0);
+    rep(i, n) {
+        if (dist[i + 1] - dist[i])
+            cout << "0 ";
+        else
+            cout << "1 ";
+    }
+    cout << endl;
+}
