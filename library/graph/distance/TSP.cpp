65ef4b5d2aa405ea7d305039e5d97684b5197fca
masayoshi64 <sho32001025@gmail.com>
2021-09-02 11:21:16 +0900

    iroiro

--- library/graph/distance/TSP.cpp
+++ library/graph/distance/TSP.cpp
@@ -1,56 +1,25 @@
-
-//オーバーフローに注意
-
-// ハミルトン閉路
+#include "library/template/template.cpp"
+/**
+ * @brief travelling salesman problem
+ * @arg graph, start
+ * @docs docs/TSP.md
+ * @details cycle: dp[all][s], path from s to t: dp[all^(1<<s)][t]
+ */
 template <typename T>
-T TSP1(vector<vector<T>> dist) {
-    int n = dist.size();
+vector<vector<T>> TSP(Graph<T> g, int s = 0) {
+    int n = g.g.size();
     T TINF = numeric_limits<T>::max();
     vector<vector<T>> dp(1 << n, vector<T>(n, TINF / 2));
-    dp[0][0] = 0;
-    rep(s, (1 << n)) {
+    dp[0][s] = 0;
+    int all = (1 << n) - 1;
+    rep(visited, all) {
         rep(pv, n) {
-            rep(nv, n) {
-                chmin(dp[s | (1 << nv)][nv], dp[s][pv] + dist[pv][nv]);
-                chmin(dp[s | (1 << nv)][nv], TINF / 2);
+            if (dp[S][pv] == TINF) continue;
+            for (auto &e : g.g[pv]) {
+                if ((S & (1 << e.to)) == 0) continue;
+                chmin(dp[S][e.to], dp[S ^ (1 << e.to)][e.from] + e.cost);
             }
         }
     }
-    return vmin(dp[(1 << n) - 1]);
-}
-
-// ハミルトンパス from st
-template <typename T>
-T TSP2(vector<vector<T>> dist, int st) {
-    int n = dist.size();
-    T TINF = numeric_limits<T>::max();
-    vector<vector<T>> dp(1 << n, vector<T>(n, TINF / 2));
-    dp[(1 << st)][st] = 0;
-    rep(s, (1 << n)) {
-        rep(pv, n) {
-            rep(nv, n) {
-                chmin(dp[s | (1 << nv)][nv], dp[s][pv] + dist[pv][nv]);
-                chmin(dp[s | (1 << nv)][nv], TINF / 2);
-            }
-        }
-    }
-    return vmin(dp[(1 << n) - 1]);
-}
-
-// ハミルトンパス from anywhere
-template <typename T>
-T TSP3(vector<vector<T>> dist) {
-    int n = dist.size();
-    T TINF = numeric_limits<T>::max();
-    vector<vector<T>> dp(1 << n, vector<T>(n, TINF / 2));
-    rep(s, n) dp[(1 << s)][s] = 0;
-    rep(s, (1 << n)) {
-        rep(pv, n) {
-            rep(nv, n) {
-                chmin(dp[s | (1 << nv)][nv], dp[s][pv] + dist[pv][nv]);
-                chmin(dp[s | (1 << nv)][nv], TINF / 2);
-            }
-        }
-    }
-    return vmin(dp[(1 << n) - 1]);
+    return dp;
 }
